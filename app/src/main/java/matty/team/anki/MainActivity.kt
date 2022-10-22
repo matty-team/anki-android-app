a4f2049b5cada01133376c827ec97fbc9013763f
Alexander Babenko <asrbbnk@gmail.com>
2022-10-22 17:21:24 +0300

    Create floating action menu to add cards and decks

--- app/src/main/java/matty/team/anki/MainActivity.kt
+++ app/src/main/java/matty/team/anki/MainActivity.kt
@@ -1,43 +1,36 @@
 package matty.team.anki
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import matty.team.anki.ui.theme.AnkiTheme
 
+private const val TAG = "MainActivity"
+
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val (isCreationMenuExpanded, setCreationMenuExpanded) = remember { mutableStateOf(false) }
             AnkiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold(floatingActionButton = {
+                    CreationMenu(
+                        isCreationMenuExpanded,
+                        setCreationMenuExpanded,
+                        onAddCardClick = { Log.d(TAG, "onCreate: onAddCardClicked") },
+                        onAddDeckClick = { Log.d(TAG, "onCreate: onAddDeckClicked") })
+                }) {
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AnkiTheme {
-        Greeting("Android")
     }
 }
